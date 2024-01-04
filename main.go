e70d1818b2729dc0c8700117a2a001dd1030f486
xiaoao98 <1149094097@qq.com>
2024-01-04 12:07:00 -0800

    feat:33forleetcode75

--- main.go
+++ main.go
@@ -9,9 +9,17 @@ import (
 func main() {
 	// s := "3[a]2[bc]"
 	// fmt.Print(leetcode75.DecodeString(s))
-	a := &leetcode75.ListNode{Val: 1, Next: nil}
+	// a := &leetcode75.ListNode{Val: 1, Next: nil}
+	// b := &leetcode75.ListNode{Val: 2, Next: nil}
+	// a.Next = b
+	// output := leetcode75.ReverseList(a)
+	// fmt.Print(output.Val, output.Next.Val, output.Next)
+	a := &leetcode75.ListNode{Val: 4, Next: nil}
 	b := &leetcode75.ListNode{Val: 2, Next: nil}
+	c := &leetcode75.ListNode{Val: 2, Next: nil}
+	d := &leetcode75.ListNode{Val: 3, Next: nil}
 	a.Next = b
-	output := leetcode75.ReverseList(a)
-	fmt.Print(output.Val, output.Next.Val, output.Next)
+	b.Next = c
+	c.Next = d
+	fmt.Print(leetcode75.PairSum(a))
 }
