f0d74fd6eb6587246b3b0b9f22b32e7882dd12d6
xiaoao98 <1149094097@qq.com>
2024-02-05 11:44:49 -0800

    feat:46forleetcode75

--- leetcode75/46.go
+++ leetcode75/46.go
@@ -3,16 +3,31 @@ package leetcode75
 func minReorder(n int, connections [][]int) int {
 	ret := 0
 	var dfs46 func(i int)
+	seen := make([]bool, n)
+	dic1 := make([][]int, n)
+	dic2 := make([][]int, n)
+	for _, connection := range connections {
+		former := connection[0]
+		latter := connection[1]
+		dic1[former] = append(dic1[former], latter)
+		dic2[latter] = append(dic2[latter], former)
+	}
 	dfs46 = func(i int) {
-		for _, connection := range connections {
-			if connection[0] == i {
-				dfs46(connection[1])
-			} else if connection[1] == i {
+		for _, ele := range dic1[i] {
+			if !seen[ele] {
 				ret += 1
-				dfs46(connection[0])
+				seen[ele] = true
+				dfs46(ele)
+			}
+		}
+		for _, ele := range dic2[i] {
+			if !seen[ele] {
+				seen[ele] = true
+				dfs46(ele)
 			}
 		}
 	}
+	seen[0] = true
 	dfs46(0)
 	return ret
 }
